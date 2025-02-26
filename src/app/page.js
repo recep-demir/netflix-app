686feedefa4ecce0d15ffa9d122e6411ae099ca9
recep-demir <demir.rp@gmail.com>
2025-02-26 19:18:45 +0100

    .

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,14 @@
 
 export default function Home() {
   return (
-    <div className="relative h-screen w-full  ">
+    <div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-fixed bg-no-repeat bg-cover ">
       <div className="bg-black w-full h-full bg-opacity-50">
-       Home
+        <div className="relative top-2/4 m-auto text-white text-center">
+          <h1 className="text-5xl font-[900] mb-3">Unlimited movies, TV shows, and more</h1>
+          <p className="text-2xl font-[400]">Watch anywhere. Cancel anytime.</p>
+
+          <button className="bg-danger w-[250px]">Get Started</button>
+        </div>
       </div>
     </div>
   );
