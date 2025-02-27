65e868142bd8fa91d0d01252579c6b942d913b12
recep-demir <demir.rp@gmail.com>
2025-02-27 21:39:13 +0100

    ait getstardet button

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
-
+"use client";
 //? app folder içindeki page.js "/" route olan home sayfasıdır
 
+import { useAuthContext } from "@/context/AuthContext";
 import Link from "next/link";
 
 //? nextjs'de tüm componentler default olarak server componenttir. client component kullanmak için "use-client" direktifini kullanmamız gerekir
 
 export default function Home() {
+  const { currentUser } = useAuthContext();
+
+  
   return (
     <div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-fixed bg-no-repeat bg-cover ">
       <div className="bg-black w-full h-full bg-opacity-50">
@@ -16,8 +20,8 @@ export default function Home() {
           <p className="text-2xl font-[400]">Watch anywhere. Cancel anytime.</p>
 
           {/* //? next/link arka planda sayfayı önceden fetch edilir. Bu, client tarafı gezintilerin performansını iyileştirmek için kullanışlıdır. Görünüm alanındaki herhangi bir <Link />  önceden yüklenecektir. */}
-          <Link href="/register">
-            <button className="btn-danger w-[250px]">Get Started</button>
+          <Link href={currentUser ? "/movies" : "/register"}>
+                      <button className="btn-danger w-[250px]">Get Started</button>
           </Link>
         </div>
       </div>
