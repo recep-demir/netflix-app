6227447d1b3b8d48c69a8b95f614f73e00aaaead
recep-demir <demir.rp@gmail.com>
2025-02-27 20:16:09 +0100

    edit moviecard

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    //? farklı domainlerden alınan image'ler için ilgili domainler config dosyasında belirtilmelidir.
+  
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "image.tmdb.org",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
--- src/app/(private)/movies/components/MovieCard.jsx
+++ src/app/(private)/movies/components/MovieCard.jsx
@@ -1,16 +1,25 @@
+
+"use client"
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import React from 'react'
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const MovieCard = ({vote_average, poster_path,id}) => {
-  return (
-    <div>
+ 
+ const router=useRouter()
+ 
+    return (
+    <div className='w-40 h-[240px] relative cursor-pointer'
+    onClick={()=>router.push("/movies/"+id)}
+    
+    >
     
     <Image
     width={160}
     height={240}
      src={IMG_API+poster_path} alt="" />
     
-    <span>
+    <span className='absolute bottom-1 right-1 text-white font-semibold z-10'>
         {vote_average.toFixed(1)}
     </span>
     </div>
